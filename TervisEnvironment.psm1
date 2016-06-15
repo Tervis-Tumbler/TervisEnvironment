c159b5d69295fb81b692d4c0f8080885203ac644
ChrisMagnuson <hourglasssand@gmail.com>
2016-06-15 08:28:23 -0400

    Initial Commit

---
+++ TervisEnvironment.psm1
@@ -0,0 +1,32 @@
+﻿$Environments = [pscustomobject][ordered]@{
+    Name="Delta"
+    Prefix="DLT"
+},
+[pscustomobject][ordered]@{
+    Name="Epsilon"
+    Prefix="EPS"
+},
+[pscustomobject][ordered]@{
+    Name="Production"
+    Prefix="PRD"
+},
+[pscustomobject][ordered]@{
+    Name="Infrastructure"
+    Prefix="INF"
+}
+
+function get-TervisEnvironmentPrefix {
+    param(
+        [Parameter(Mandatory)][string]$EnvironmentName
+    )
+    $Prefix = $Environments | 
+    where Name -eq $EnvironmentName | 
+    select -ExpandProperty Prefix
+
+    if($Prefix) {$Prefix} else { Throw "No Environment prefix found for $EnvironmentName" }
+}
+
+function Get-TervisEnvironmentName {
+    $Environments |
+    Select -ExpandProperty Name
+}
